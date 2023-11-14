3b323cd7b3c1d10152a076d1a9ee264b6c078180
Movits <robertomovits@gmail.com>
2023-11-14 11:44:18 -0300

    uso de api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const API = "https://pokeapi.co/api/v2/pokemon/ditto";
   const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState("def nombre");
+  const [img, setImg] = useState("def img");
+
+  useEffect(()=>{
+    async function fectchApi() {
+      const res = await fetch(API);
+      let info = "def info";
+      if (res.ok) {
+        info = await res.json();
+        setNombre(info.name)
+        setImg(info.sprites.front_default);
+      }
+    }
+
+    fectchApi();
+  });
 
   function aumentar() {
     setContador(contador+1);
@@ -13,6 +30,8 @@ function App() {
       <h1>Hola!</h1>
       <button onClick={aumentar}>Boton</button>
       <h2>{contador}</h2>
+      <h2>{nombre}</h2>
+      <img src={img} alt="" />
     </>
   )
 }
